047a2a19f394ac1671544ac96c6df28de4968f06
Teemu Y <theme87@gmail.com>
2015-09-06 21:30:58 +0300

    dokumentaatio.pdf lisätty

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -4,7 +4,8 @@
 
     public static function index(){
       // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-   	  View::make('home.html');
+      //echo 'tämä on etusivu';
+      View::make('home.html');
     }
 
     public static function sandbox(){
