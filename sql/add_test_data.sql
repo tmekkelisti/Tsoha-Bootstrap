b491a9c2477a3ed778f290ac150d67ea3666401d
Teemu Y <theme87@gmail.com>
2015-09-08 01:39:54 +0300

    viikko2 done

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,13 @@
 -- Lisää INSERT INTO lauseet tähän tiedostoon
+INSERT INTO Kayttaja (user_name, user_password, user_added, user_admin) VALUES
+('temekkelisti', 'mitalli', NOW(), false);
+INSERT INTO Kayttaja (user_name, user_password, user_added, user_admin) VALUES
+('tmekk', 'mitalli1', NOW(), true);
+
+INSERT INTO Category (cat_name) VALUES ('testikategoria');
+
+INSERT INTO Topic (topic_topic, topic_content, topic_added) VALUES
+('eka topikki', 'tää on kontentti', NOW());
+
+INSERT INTO Reply (reply_content, reply_added) VALUES
+('eka vastaus', NOW());
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,30 @@
 -- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE Kayttaja(
+	id SERIAL PRIMARY KEY,
+	user_name varchar(20) NOT NULL,
+	user_password varchar(50) NOT NULL,
+	user_added TIMESTAMP,
+	user_admin boolean DEFAULT FALSE
+);
+
+CREATE TABLE Category(
+	id SERIAL PRIMARY KEY,
+	cat_name varchar(50) NOT NULL
+);
+
+CREATE TABLE Topic(
+	id SERIAL PRIMARY KEY,
+	topic_topic varchar(50) NOT NULL,
+	topic_content text NOT NULL,
+	topic_added TIMESTAMP,
+	kayttaja_id INTEGER REFERENCES Kayttaja(id),
+	category_id INTEGER REFERENCES Category(id)
+);
+
+CREATE TABLE Reply(
+	id SERIAL PRIMARY KEY,
+	reply_content text NOT NULL,
+	reply_added TIMESTAMP,
+	kayttaja_id INTEGER REFERENCES Kayttaja(id),
+	topic_id INTEGER REFERENCES Topic(id)
+);
